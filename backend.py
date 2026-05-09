ef8315d6be69bd20b16d51be00a30134435c9794
hari <harithota609@gmail.com>
2026-05-09 12:25:41 +0530

    changes langchain code

--- backend.py
+++ backend.py
@@ -20,7 +20,7 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters  import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
